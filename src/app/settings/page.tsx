4acb756651d424e9f5e91289e4b178d194c74aa2
iamzainn <iamzainnf@gmail.com>
2024-05-15 14:58:47 +0500

    fix auth

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,17 @@
+// import prisma from "@/lib/db"
+
+const getData = async()=>{
+    
+}
+
+
+const SettingPage = async() => {
+    // console.log(await getData());
+  return (
+    <div className='setting'>
+      Setting
+    </div>
+  )
+}
+
+export default SettingPage
